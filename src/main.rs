b4931aea2a2fe7efd147d0b744c8a802d265a506
Aleksandr Lisianoi <alisianoi@protonmail.com>
2020-10-03 11:09:30 +0200

    Configure logging colors and spaces

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use log::{debug, info, warn};
+use log::{debug, error, info, trace, warn};
 use std::env;
 use std::env::VarError;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     setup_logger()?;
+
     debug!("Hello, world!");
 
     let var_alex = match env::var("ALEX") {
@@ -22,23 +23,40 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     info!("The $ALEX var is {:#?}", var_alex);
 
+    trace!("Here is a trace");
+    debug!("Here is a debug");
+    info!("Here is an info");
+    warn!("Here is a warning");
+    error!("Here is an error");
+
     Ok(())
 }
 
 fn setup_logger() -> Result<(), fern::InitError> {
     let colors = fern::colors::ColoredLevelConfig::new()
-        .info(fern::colors::Color::Green);
+        .trace(fern::colors::Color::BrightWhite)
+        .debug(fern::colors::Color::White)
+        .info(fern::colors::Color::Cyan)
+        .warn(fern::colors::Color::Magenta)
+        .error(fern::colors::Color::Red);
+
     fern::Dispatch::new()
         .format(move |out, message, record| {
+            let level = record.level();
             out.finish(format_args!(
-                "{} {} {} {}",
-                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-                record.target(),
-                colors.color(record.level()),
-                message
+                "{color_lft}{date}{color_rgt} {target} {color_lft}{level}{color_rgt} {message}",
+                color_lft = format_args!(
+                    "\x1B[{}m",
+                    colors.get_color(&level).to_fg_str()
+                ),
+                color_rgt = "\x1B[0m",
+                date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                target = record.target(),
+                level = format!("{:5}", level),
+                message = message
             ))
         })
-        .level(log::LevelFilter::Debug)
+        .level(log::LevelFilter::Trace)
         .chain(std::io::stdout())
         .apply()?;
     Ok(())
