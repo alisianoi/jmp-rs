442a07bde747e71b9c3cb421cc842a7effd3ffd8
Aleksandr Lisianoi <alisianoi@protonmail.com>
2020-10-09 11:02:10 +0200

    Move setup_logger into logger.rs

---
+++ src/logger.rs
@@ -0,0 +1,34 @@
+use crate::config::Config;
+
+pub fn setup_logger(config: &Config) -> Result<(), fern::InitError> {
+    let colors = fern::colors::ColoredLevelConfig::new()
+        .trace(fern::colors::Color::BrightWhite)
+        .debug(fern::colors::Color::White)
+        .info(fern::colors::Color::Cyan)
+        .warn(fern::colors::Color::Magenta)
+        .error(fern::colors::Color::Red);
+
+    let level = config.level;
+
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            let level = record.level();
+            out.finish(format_args!(
+                "{color_lft}{date}{color_rgt} {target} {color_lft}{level}{color_rgt} {message}",
+                color_lft = format_args!(
+                    "\x1B[{}m",
+                    colors.get_color(&level).to_fg_str()
+                ),
+                color_rgt = "\x1B[0m",
+                date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                target = record.target(),
+                level = format!("{:5}", level),
+                message = message
+            ))
+        })
+        .level(level)
+        .chain(std::io::stdout())
+        .apply()?;
+
+    Ok(())
+}
