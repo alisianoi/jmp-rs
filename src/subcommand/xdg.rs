7d306b3f4e0dd689d72214c8b60558802cbbb48b
Aleksandr Lisianoi <alisianoi@protonmail.com>
2020-10-10 12:15:58 +0200

    Echo a bunch of XDG variables
    See: https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html

--- src/subcommand/xdg.rs
+++ src/subcommand/xdg.rs
@@ -1,6 +1,7 @@
 use crate::config::Config;
+use crate::env::var_or_default;
 use crate::logger::setup_logger;
-use log::{debug, error, info, trace, warn};
+use log::trace;
 
 use clap::ArgMatches;
 
@@ -11,6 +12,24 @@ pub fn run(matches: &ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
     setup_logger(&config)?;
 
     trace!("Enter run()");
+    // Default is: ${HOME}/.local/share
+    let (_var_xdg_data_home, _var_xdg_data_home_set) =
+        var_or_default("XDG_DATA_HOME");
+    // Default is: ${HOME}/.config
+    let (_var_xdg_config_home, _var_xdg_config_home_set) =
+        var_or_default("XDG_CONFIG_HOME");
+    // Default is: /usr/local/share:/usr/share
+    let (_var_xdg_data_dirs, _var_xdg_data_dirs_set) =
+        var_or_default("XDG_DATA_DIRS");
+    // Default is: /etc/xdg
+    let (_var_xdg_config_dirs, _var_xdg_config_dirs_set) =
+        var_or_default("XDG_CONFIG_DIRS");
+    // Default is: ${HOME}/.cache
+    let (_var_xdg_cache_home, _var_xdg_cache_home_set) =
+        var_or_default("XDG_CACHE_HOME");
+    // Default is: no default, the app chooses a directory on its own
+    let (_var_xdg_runtime_dir, _var_xdg_runtime_dir_set) =
+        var_or_default("XDG_RUNTIME_DIR");
     trace!("Leave run() (Ok)");
 
     Ok(())
